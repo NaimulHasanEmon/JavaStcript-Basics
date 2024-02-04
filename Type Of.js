e31b16f04aa64ee0c7429a3414a037871e9b0ea0
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2024-02-04 23:38:13 +0600

    Add files via upload

---
+++ Type Of.js
@@ -0,0 +1,13 @@
+let name = "Emon";
+let age = 23;
+let salary = 1000.50;
+let isStudent = true;
+
+console.log(name);
+console.log(typeof name);
+console.log(age);
+console.log(typeof age);
+console.log(salary);
+console.log(typeof salary);
+console.log(isStudent);
+console.log(typeof isStudent);
