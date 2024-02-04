ef7abe51bd3b4ad22aaf3269d502c050d65b751b
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2024-02-05 00:05:54 +0600

    Add files via upload

---
+++ Slice.js
@@ -0,0 +1,4 @@
+let fullName = "Naimul";
+console.log(fullName);
+let partName = fullName.slice(1, 5);
+console.log(partName);
---
+++ Split.js
@@ -0,0 +1,6 @@
+let fullName = "Naimul Hasan Emon";
+console.log("Full Name: " + fullName);
+console.log(fullName.split());
+console.log(fullName.split(''));
+console.log(fullName.split(' '));
+console.log(fullName.split('a'));
