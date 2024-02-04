bd678f21bcec33631e1a31abbc7bc11b8e361a59
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2024-02-05 01:04:51 +0600

    Add files via upload

---
+++ Join.js
@@ -0,0 +1,4 @@
+let identity = [ 'I', 'am', 'an', 'undergraduate', 'student', 'of', 'CSE', 'at', 'Daffodil']
+console.log(identity.join());
+console.log(identity.join(''));
+console.log(identity.join(' '));
