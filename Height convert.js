3055c6b29a79581ecb467fe31a7a6184be74e7a5
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-07 02:52:54 +0600

    Update Height convert.js

--- Height convert.js
+++ Height convert.js
@@ -3,5 +3,6 @@ function inchToFeet(inch) {
     let feet = inch / 12;
     let inches = inch % 12;
     console.log("Your height is: ", Math.round(feet), "Feet", inches, "Inches");
+    // Instead of using Math.rount(feet), you can use parseInt(feet) too
 }
 inchToFeet(prompt('Enter your height in inch: '));
