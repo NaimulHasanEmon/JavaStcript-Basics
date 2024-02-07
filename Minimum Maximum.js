4daf1aa35ad188a74dd539f5e56f434d32d818c0
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-07 10:46:12 +0600

    Add files via upload

---
+++ Minimum Maximum.js
@@ -0,0 +1,4 @@
+let minNum = Math.min(23, 234, 13, -24, 2, 0.54, 143);
+let maxNum = Math.max(23, 234, 13, -24, 2, 0.54, 143);
+console.log('Minimum number is: ' + minNum);
+console.log("Minimum number is: " + maxNum);
