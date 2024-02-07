733226bfd9c5fda8bf1f2d604714e4c5b2902ed0
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-07 11:08:06 +0600

    Add files via upload

---
+++ Math (Random).js
@@ -0,0 +1,8 @@
+// Will give the random number from 0 to 1
+console.log(Math.random());
+
+// Will give the random number from 0 to 5
+console.log(Math.random() * 5);
+
+// Will give the random number from 0 to 10 in integer (including 10)
+console.log(Math.round(Math.random() * 10));
