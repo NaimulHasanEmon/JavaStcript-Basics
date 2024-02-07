98521ad6f59f3eb96c9b362f25b1a7b54445ea57
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-07 11:00:36 +0600

    Add files via upload

---
+++ Math (Floor).js
@@ -0,0 +1,4 @@
+// This function will return the decimal number only, doesn't matter even if the decimal number is 0.99
+console.log(Math.floor(4.25)); // Output: 4
+console.log(Math.floor(2.50)); // Output: 2
+console.log(Math.floor(6.99)); // Output: 6
