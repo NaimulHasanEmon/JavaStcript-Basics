5b2b5c4be22e96f9358b5dcb446fbca1ba4b239b
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-07 10:56:24 +0600

    Add files via upload

---
+++ Math (Round).js
@@ -0,0 +1,5 @@
+// If the number in the decimal point is less than 0.5 then the number will not increase
+// If the number in the decimal point is more or equal to 0.5 then the number will increase by 1
+console.log(Math.round(4.25)); // Output: 4
+console.log(Math.round(4.50)); // Output: 5
+console.log(Math.round(4.99)); // Output: 5
