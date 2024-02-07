600be2d4b90f632d2db348a5e6a826fc10b6e55b
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-07 11:02:49 +0600

    Add files via upload

---
+++ Math (Ceil).js
@@ -0,0 +1,4 @@
+// This function will return decimal number increased by 1, doesn't matter even if the decimal number is 0.01
+console.log(Math.ceil(4.01)); // Output: 5
+console.log(Math.ceil(2.50)); // Output: 3
+console.log(Math.ceil(6.95)); // Output: 7
